bd3d3cffcc2616e3b83f0db279ecc56b1c0cada8
Ahmed Tawfiq <ahmedadelmm26@gmail.com>
2022-02-12 19:59:40 +0200

    optimize  PascalTriangle

--- src/pascale_triangle/PascalTriangle.kt
+++ src/pascale_triangle/PascalTriangle.kt
@@ -1,8 +1,7 @@
 package pascale_triangle
 
 /*
-    https://leetcode.com/problems/pascals-triangle/
-
+ https://leetcode.com/problems/pascals-triangle/
  */
 object PascalTriangle {
 
@@ -12,7 +11,6 @@ object PascalTriangle {
         rows.add(arrayOf(1))
 
         for (currentItem in 2..numRows) {
-
             val lastRow = rows[currentItem - 2]
             val currentRow = Array(currentItem) { 1 }
 
@@ -24,10 +22,8 @@ object PascalTriangle {
                 val sum = lastRow[item - 1] + lastRow[item - 2]
                 currentRow[item - 1] = sum
             }
-
             rows.add(currentRow)
         }
-
         return rows
     }
 
