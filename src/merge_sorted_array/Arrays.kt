cf66f14c8b00c1720a7e8f4d81178acde797b077
Ahmed Tawfiq <ahmedadelmm26@gmail.com>
2022-02-19 22:29:04 +0200

    Add Throws EXception for definsive programming with Merge values

--- src/merge_sorted_array/Arrays.kt
+++ src/merge_sorted_array/Arrays.kt
@@ -4,12 +4,14 @@ object Arrays {
     //https://leetcode.com/problems/merge-sorted-array/
     private fun merge(nums1: IntArray, m: Int, nums2: IntArray, n: Int) {
         if ((m + n) <= 0) throw IllegalStateException()
+        if (nums1.size != (m + n)) throw IndexOutOfBoundsException()
+        if (nums2.size != n) throw IndexOutOfBoundsException()
 
         var pointer1 = m - 1
         var pointer2 = n - 1
         var position = (m + n) - 1
 
-        while (pointer2 >= 0) {
+        while (pointer2 >= 0) {  //Time Complexity: O(n)
             if (pointer1 >= 0 && nums1[pointer1] > nums2[pointer2])
                 nums1[position--] = nums1[pointer1--]
             else
