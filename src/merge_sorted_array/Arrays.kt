8b1b615c86431fc0d42d9f17a89778b13c76e541
Ahmed Tawfiq <ahmedadelmm26@gmail.com>
2022-02-19 22:05:39 +0200

    Check Pointer 1 in While Firstly

--- src/merge_sorted_array/Arrays.kt
+++ src/merge_sorted_array/Arrays.kt
@@ -1,26 +1,36 @@
 package merge_sorted_array
 
 object Arrays {
-    fun merge(nums1: IntArray, m: Int, nums2: IntArray, n: Int) {
-        //After Merge
-        if (nums1.size != (m + n)) throw IllegalStateException()
+    private fun merge(nums1: IntArray, m: Int, nums2: IntArray, n: Int) {
+        if ((m + n) <= 0) throw IllegalStateException()
 
-        //Input: nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3
-        val pointer1 = m - 1
-        val pointer2 = n - 1
-        val position = (m + n) - 1
+        var pointer1 = m - 1
+        var pointer2 = n - 1
+        var position = (m + n) - 1
 
-        while (pointer1 <= 0) {
-            if (nums1[pointer1] <= nums2[pointer2]) {
-                nums1[position] = nums2[pointer2]
-            } else {
-                nums1[position] = nums1[pointer1]
-            }
+        while (pointer2 >= 0) {
+            if (pointer1 >= 0 && nums1[pointer1] > nums2[pointer2])
+                nums1[position--] = nums1[pointer1--]
+            else
+                nums1[position--] = nums2[pointer2--]
         }
     }
 
     @JvmStatic
     fun main(args: Array<String>) {
-        //merge()
+        //Input: nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3
+        val num1 = IntArray(6) { 0 }
+        num1[0] = 1
+        num1[1] = 2
+        num1[2] = 3
+
+        val num2 = IntArray(3)
+        num2[0] = 2
+        num2[1] = 5
+        num2[2] = 6
+
+        merge(num1, 3, num2, 3)
+
+        print(num1.contentToString())
     }
 }
