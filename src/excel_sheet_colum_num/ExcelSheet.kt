f4b3450ac8e4e3a90255233501c3798a23934b7d
Ahmed Tawfiq <ahmedadelmm26@gmail.com>
2022-02-28 00:47:33 +0200

    Optimize ExcelSheet

--- src/excel_sheet_colum_num/ExcelSheet.kt
+++ src/excel_sheet_colum_num/ExcelSheet.kt
@@ -4,9 +4,6 @@ package excel_sheet_colum_num
 object ExcelSheet {
 
     private fun titleToNumber(columnTitle: String): Int {
-        if (columnTitle.isEmpty()) throw IndexOutOfBoundsException()
-        if (columnTitle.length > 7) throw IndexOutOfBoundsException()
-
         var result = 0  //result*26 + (ASSCi Code)
         for (char in columnTitle)
             result = (result * 26) + (char - 'A') + 1
@@ -18,3 +15,38 @@ object ExcelSheet {
         println(titleToNumber("AB"))
     }
 }
+/*
+Given a string columnTitle that represents the column title as appear in an Excel sheet, return its corresponding column number.
+
+For example:
+
+A -> 1
+B -> 2
+C -> 3
+...
+Z -> 26
+AA -> 27
+AB -> 28
+...
+
+
+Example 1:
+
+Input: columnTitle = "A"
+Output: 1
+Example 2:
+
+Input: columnTitle = "AB"
+Output: 28
+Example 3:
+
+Input: columnTitle = "ZY"
+Output: 701
+
+
+Constraints:
+
+1 <= columnTitle.length <= 7
+columnTitle consists only of uppercase English letters.
+columnTitle is in the range ["A", "FXSHRXW"].
+ */
