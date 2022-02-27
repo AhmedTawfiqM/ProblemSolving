1eacb6aa9a353ce0cdea416423f864d00a1ede4f
Ahmed Tawfiq <ahmedadelmm26@gmail.com>
2022-02-28 00:51:18 +0200

    Optimize ExcelSheetBP

--- src/excel_sheet_colum_num/ExcelSheetBP.kt
+++ src/excel_sheet_colum_num/ExcelSheetBP.kt
@@ -4,28 +4,33 @@ import java.lang.UnsupportedOperationException
 
 //https://leetcode.com/problems/excel-sheet-column-number/
 object ExcelSheetBP {
-    private const val TotalCharsNums = 26
-    private const val AChar = 'A'
+    private const val totalCharsNums = 26
+    private const val plussedNumber = 1
+    private const val aChar = 'A'
+    private const val maximumLength = 7
 
     private fun titleToNumber(columnTitle: String): Int {
-        if (columnTitle.isEmpty()) throw IndexOutOfBoundsException()
-        if (columnTitle.length > 7) throw IndexOutOfBoundsException()
+        if (columnTitle.isEmpty())
+            throw IndexOutOfBoundsException()
+        if (columnTitle.length > maximumLength)
+            throw IndexOutOfBoundsException()
 
         var result = 0  //result*26 + (ASSCi Code)
         for (char in columnTitle) {
-            if (char.isLowerCase()) throw UnsupportedOperationException()
-            result *= TotalCharsNums
-            result += computeAssciCode(char) + 1
+            if (char.isLowerCase())
+                throw UnsupportedOperationException()
+            result *= totalCharsNums
+            result += computeAssciCode(char) + plussedNumber
         }
         return result
     }
 
     private fun computeAssciCode(char: Char): Int {
-        return char - AChar
+        return char - aChar
     }
 
     @JvmStatic
     fun main(args: Array<String>) {
-        println(titleToNumber("AaB"))
+        println(titleToNumber("ACB"))
     }
 }
