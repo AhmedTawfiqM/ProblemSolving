a569265d5f85bcb51f4ade5ee37c8252e68a5a03
Ahmed Tawfiq <ahmedadelmm26@gmail.com>
2022-03-14 15:00:52 +0200

    Add description from Leetcode

--- src/common_prefix/CommonPrefix.kt
+++ src/common_prefix/CommonPrefix.kt
@@ -1,5 +1,6 @@
 package common_prefix
 
+//https://leetcode.com/problems/longest-common-prefix/
 object CommonPrefix {
 
     private fun longestCommonPrefix(list: Array<String>): String {
@@ -17,6 +18,29 @@ object CommonPrefix {
     @JvmStatic
     fun main(args: Array<String>) {
         println(longestCommonPrefix(arrayOf("flower", "flow", "flight")))
-        println(longestCommonPrefix(arrayOf("dog","racecar","car")))
+        println(longestCommonPrefix(arrayOf("dog", "racecar", "car")))
+        println(longestCommonPrefix(arrayOf("ahmed", "sahm", "wassbah")))
     }
 }
+/*
+ * Write a function to find the longest common prefix string amongst an array of strings.
+
+If there is no common prefix, return an empty string "".
+
+Example 1:
+
+Input: strs = ["flower","flow","flight"]
+Output: "fl"
+Example 2:
+
+Input: strs = ["dog","racecar","car"]
+Output: ""
+Explanation: There is no common prefix among the input strings.
+
+
+Constraints:
+
+1 <= strs.length <= 200
+0 <= strs[i].length <= 200
+strs[i] consists of only lower-case English letters.
+ **/
